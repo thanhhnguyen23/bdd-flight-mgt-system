b81e921f804def0e059a5cd2c3e0a2601201bce9
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-04-05 11:13:54 -0700

    add VIP passenger scenario to business flights

--- src/test/java/com/pluralsight/bddfundamentals/airport/PassengersPolicy.java
+++ src/test/java/com/pluralsight/bddfundamentals/airport/PassengersPolicy.java
@@ -8,13 +8,15 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PassengersPolicy {
+
     private Flight economyFlight;
+    private Flight businessFlight;
     private Passenger mike;
+    private Passenger john;
 
     @Given("there is an economy flight")
     public void thereIsAnEconomyFlight() {
         economyFlight = new EconomyFlight("1");
-
     }
 
     @When("we have a usual passenger")
@@ -34,4 +36,54 @@ public class PassengersPolicy {
                 () -> assertEquals(0, economyFlight.getPassengersList().size())
         );
     }
+    //--------------------------------------------------------------------------------
+    @Given("there is an business flight")
+    public void thereIsAnBusinessFlight() {
+        businessFlight = new BusinessFlight("2");
+    }
+
+    @When("we have a VIP passenger")
+    public void weHaveAVIPPassenger() {
+        john = new Passenger("John", true);
+    }
+
+    @Then("you can add him but cannot remove him from an economy flight")
+    public void youCanAddHimButCannotRemoveHimFromAnEconomyFlight() {
+
+        assertAll("Verify all conditions for a VIP passenger in an economy flight",
+                () -> assertEquals("1", economyFlight.getId()),
+                () -> assertEquals(true, economyFlight.addPassenger(john)),
+                () -> assertEquals(1, economyFlight.getPassengersList().size()),
+                () -> assertEquals("John", economyFlight.getPassengersList().get(0).getName()),
+                () -> assertEquals(false, economyFlight.removePassenger(john)),
+                () -> assertEquals(1, economyFlight.getPassengersList().size())
+        );
+    }
+
+    //--------------------------------------------------------------------------------
+    @Given("there is a business flight")
+    public void thereIsABusinessFlight() {
+        businessFlight = new BusinessFlight("2");
+    }
+
+    @Then("you cannot add or remove him from a business flight")
+    public void youCannotAddOrRemoveHimFromABusinessFlight() {
+        assertAll("Verify all conditions for a usual passenger on a business flight",
+                () -> assertEquals(false, businessFlight.addPassenger(mike)),
+                () -> assertEquals(0, businessFlight.getPassengersList().size()),
+                () -> assertEquals(false, businessFlight.removePassenger(mike)),
+                () -> assertEquals(0, businessFlight.getPassengersList().size())
+        );
+    }
+
+    @Then("you can add him but cannot remove him from a business flight")
+    public void youCanAddHimButCannotRemoveHimFromABusinessFlight() {
+        assertAll("Verify all conditions for a VIP passenger in a business flight",
+                () -> assertEquals(true, businessFlight.addPassenger(john)),
+                () -> assertEquals(1, businessFlight.getPassengersList().size()),
+                () -> assertEquals(false, businessFlight.removePassenger(john)),
+                () -> assertEquals(1, businessFlight.getPassengersList().size())
+        );
+    }
+
 }
