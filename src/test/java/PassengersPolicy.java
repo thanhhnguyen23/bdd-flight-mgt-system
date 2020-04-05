e985ae7529999578cbc1207eef3b5c3dc3762853
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-04-04 19:56:10 -0700

    add passenger policy skeleton class

---
+++ src/test/java/PassengersPolicy.java
@@ -0,0 +1,17 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class PassengersPolicy {
+    @Given("there is an economy flight")
+    public void thereIsAnEconomyFlight() {
+    }
+
+    @When("we have a usual passenger")
+    public void weHaveAUsualPassenger() {
+    }
+
+    @Then("you can add and remove him from an economy flight")
+    public void youCanAddAndRemoveHimFromAnEconomyFlight() {
+    }
+}
