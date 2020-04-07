84ddd13686eb0ea74df8bdbf4e1b8612ec886c84
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2020-04-06 23:39:28 -0700

    add code skeleton to allow passengers to accumulate points for flights

---
+++ src/main/java/com/pluralsight/bddfundamentals/airport/Mileage.java
@@ -0,0 +1,18 @@
+package com.pluralsight.bddfundamentals.airport;
+
+import java.util.List;
+
+public class Mileage {
+    public void addMileage(Passenger mike, int mileage1) {
+        // TODO
+    }
+
+    public void calculateGivenPoints() {
+        // TODO
+    }
+
+    public <E> List<E> getPassengersPointsMap() {
+        // TODO
+        return null;
+    }
+}
--- src/test/java/com/pluralsight/bddfundamentals/airport/BonusPolicy.java
+++ src/test/java/com/pluralsight/bddfundamentals/airport/BonusPolicy.java
@@ -4,16 +4,28 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BonusPolicy {
+    private Passenger mike;
+    private Mileage mileage;
+
     @Given("we have a usual passenger with a mileage")
     public void weHaveAUsualPassengerWithAMileage() {
+        mike = new Passenger("Mike", false);
+        mileage = new Mileage();
     }
 
     @When("the usual passenger travels <mileage{int}> and <mileage{int}> and <mileage{int}>")
-    public void theUsualPassengerTravelsMileageAndMileageAndMileage(int arg0, int arg1, int arg2) {
+    public void theUsualPassengerTravelsMileageAndMileageAndMileage(int mileage1, int mileage2, int mileage3) {
+        mileage.addMileage(mike, mileage1);
+        mileage.addMileage(mike, mileage2);
+        mileage.addMileage(mike, mileage3);
     }
 
     @Then("the bonus points of the usual passenger should be <points>")
-    public void theBonusPointsOfTheUsualPassengerShouldBePoints() {
+    public void theBonusPointsOfTheUsualPassengerShouldBePoints(int points) {
+        mileage.calculateGivenPoints();
+        assertEquals(points, mileage.getPassengersPointsMap().get(0));
     }
 }
